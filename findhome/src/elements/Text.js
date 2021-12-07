9e9ebabd3dc15c9647dcb4261263fd6ab6913056
taeeon kim <myhom43@gmail.com>
2021-12-07 21:37:15 +0900

    margin 추가

--- findhome/src/elements/Text.js
+++ findhome/src/elements/Text.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const Text = (props) => {
 
-const {children, size, color, bold} = props;
- const styles = {size, color, bold}
+const {children, size, color, bold, margin} = props;
+ const styles = {size, color, bold, margin}
     return (
         <React.Fragment>
             <P {...styles}>{children}</P>
@@ -16,12 +16,14 @@ Text.defaultProps ={
  size : null ,
  bold : false, 
  color : "#000000",
+ margin : null,
 }
 
 const P = styled.p`
     ${(props)=>(props.bold? `font-weight: 700;`: `font-weight: 400;`)};
     ${(props)=>(`color : ${props.color};`)};
     ${(props)=> (`font-size: ${props.size};`)};
+    ${(props)=>(`margin : ${props.margin};`)};
 `;
 
 

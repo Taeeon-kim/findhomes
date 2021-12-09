4c6431d840f9982532aeef15e9cd99b171eec375
taeeon kim <myhom43@gmail.com>
2021-12-09 18:13:36 +0900

    post이미지뜨게 기본설정

--- findhome/src/redux/modules/post.js
+++ findhome/src/redux/modules/post.js
@@ -15,43 +15,46 @@ const initialState = {
         {   id: "yougnble@aa.com",
             user_name: "youngble",
             contents:"test 내용",
-            img_url: ''}
+            img_url: 'https://newsimg.hankookilbo.com/cms/articlerelease/2021/06/05/ef519975-80c8-40b6-b25a-47ab6270dc60.png'}
 
     ] 
 }
 
 const getMainAPI = () => { 
     return function (dispatch,getState,{history}){
-        const API = '3.38.169.23/api/posts'
-        axios.get(API)
-            .then((response) => { // 데이터를 가져온다 // .then은 앞에꺼 실행한 후 다음 동작을 하는 것이다. 
-             console.log(response.data)
+        // const API = '3.38.169.23/api/posts'
+        // axios.get(API)
+        //     .then((response) => { // 데이터를 가져온다 // .then은 앞에꺼 실행한 후 다음 동작을 하는 것이다. 
+        //      console.log(response.data)
              
-             let post_list = [] // 넘어가는 setPost는 배열이기때문에 각 데이터는 배열로 묶여야한다
+        //      let post_list = [] // 넘어가는 setPost는 배열이기때문에 각 데이터는 배열로 묶여야한다
             
-             response.data.forEach((_post) => { // forEach로  객체 하나하나 돌게 한다.
+        //      response.data.forEach((_post) => { // forEach로  객체 하나하나 돌게 한다.
 
-                // let fitpost = {  // post componets의 있는 데이터와 파이어스토어에 있는 데이터가 형식이 조금 달라서 맞춰야아한다.
-                //     id:fitpost.id,
-                //     ...fitpost.data() 
-                // }
-                console.log(_post.nickname)
-                let post = {
-                    _id:_post._id,
-                    user_name:_post.nickname,
-                    contents:_post.constent,
-                    img_url:_post.img_url
-                }
-                console.log(post)
+        //         // let fitpost = {  // post componets의 있는 데이터와 파이어스토어에 있는 데이터가 형식이 조금 달라서 맞춰야아한다.
+        //         //     id:fitpost.id,
+        //         //     ...fitpost.data() 
+        //         // }
+        //         console.log(_post.nickname)
+        //         let post = {
+        //             _id:_post._id,
+        //             user_name:_post.nickname,
+        //             contents:_post.constent,
+        //             img_url:_post.img_url
+        //         }
+        //         console.log(post)
 
-                post_list.push(post)
-            })
+        //         post_list.push(post)
+        //     })
 
-            console.log(post_list)
+        //     console.log(post_list)
 
-            dispatch(setPost(post_list))
+        //     dispatch(setPost(post_list))
 
-        })
+        // })
+        console.log(initialState.list)
+        dispatch(setPost(initialState.list))
+        
 
 }
         
